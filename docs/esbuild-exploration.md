17839b84c061845ec3a1f1be381dcc4a519b8258
Joshua Stephen <jostephen22@yahoo.com>
2021-03-08 21:41:32 +0700

    feat: add docs to home screen

--- docs/esbuild-exploration.md
+++ docs/esbuild-exploration.md
@@ -1,6 +1,7 @@
 ---
 title: 'Using esbuild As Your New Bundler'
-description: 'Thoughts regarding esbuild as the new replacement of bundle & webpack for developing packages.'
+description: 'Thoughts regarding esbuild as replacement of existing bundlers & babel for developing packages.'
+published: 'March 8, 2021'
 ---
 
 # Using esbuild As Your New Bundler
@@ -9,7 +10,7 @@ Published on March 8, 2021
 
 ---
 
-You might have heard about [esbuild](https://esbuild.github.io/) before even reaching this post. If you did, that's great! If you never heard about it before this, or at least you haven't tried it, **you should!**
+You might have heard about [esbuild](https://esbuild.github.io/) before even reading this post. If you did, that's great! But if you never heard about it before this, or at least you haven't tried it, then **you should!**
 
 You can make esbuild to become either a bundler, or just simply use it as a compiler in replacement of babel.
 
@@ -17,9 +18,9 @@ You can make esbuild to become either a bundler, or just simply use it as a comp
 
 We all know about babel, how powerful babel is, and how babel has helped us building great stuffs all around the web. But in for large scale project, you might have experienced that the build time takes very long! This might become one of the biggest productivity blocker.
 
-Babel simply has more overhead cost than esbuild. Even if you cache the loader, esbuild is much much faster. Although, esbuild even put [restriction](https://esbuild.github.io/plugins/#plugin-api-limitations) for when writing plugin, so you might want to know this before really replacing your babel.
+Babel simply has more overhead cost than esbuild. Even if you cache the loader, esbuild is much much faster. Although, esbuild even put [restriction](https://esbuild.github.io/plugins/#plugin-api-limitations) for when writing plugin, so you might want to know this before really replacing babel, especially if you depend on specific babel plugins that is not present in esbuild.
 
-Now let us talk about how replacing babel with esbuild. But before this, you should know that babel can simply take a folder as entry points, but in esbuild case you need to **specify** all the file paths that you are going to compile.
+Let us talk about how to replace babel with esbuild. But before this, you should know that babel can simply take a folder as entry points, but in esbuild you need to **specify** all the file paths that you are going to compile as an array of paths.
 
 I suggest you to start from using **Typescript** because esbuild supports typescript natively, without adding new plugin. Therefore, I can say that probably it doesn't have any performance impact when you are compiling typescript or normal javascript files, unlike Babel.
 
@@ -66,7 +67,7 @@ async function build() {
     entryPoints: tsConfig.fileNames,
     outdir: OUT_DIR,
     loader: {
-      '.js': 'jsx',
+      '.js': 'jsx', // if you are also compiling react files
     },
     format: 'esm',
     target: 'es2015',
@@ -111,11 +112,11 @@ async function build() {
 
 **And you're done!**
 
-If you are curious about the timing, try logging the time before each processes (build & copy).
+If you are curious about the timing, try logging the time before & after each processes (build & copy).
 
 ## Using together with existing bundler
 
-If you are using webpack, I suggest to look into [esbuild-loader](https://github.com/privatenumber/esbuild-loader).
+If you are using webpack, I suggest you to have a look into [esbuild-loader](https://github.com/privatenumber/esbuild-loader).
 
 If you are using rollup, there is also existing [plugin](https://github.com/egoist/rollup-plugin-esbuild) to pair with esbuild. You can refer to the [full configuration](https://github.com/josteph/elastic-node-example/blob/main/rollup.config.js) I have tried before writing this post.
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,96 @@
 import React from 'react';
+import Link from 'next/link';
 import { GitHub, Twitter, Linkedin } from 'react-feather';
+import { getAllDocs } from '@lib/docs';
 import styles from '@styles/home.page.module.scss';
 
-export default function Home() {
+export async function getStaticProps() {
+  const docs = getAllDocs();
+
+  return {
+    props: {
+      docs,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: false,
+  };
+}
+
+export default function Home({ docs }: { docs: any[] }) {
   return (
-    <main className="main-container">
-      <div className={styles.styMainInfo}>
-        <h3>
-          Hi, I am <span className="main-gradient">Joshua</span>
-        </h3>
-        <br />
-        <h4 className="main-gradient">
-          Software Engineer
+    <main>
+      <div className="main-container">
+        <div className={styles.styMainInfo}>
+          <h3>
+            Hi, I am <span className="main-gradient">Joshua</span>
+          </h3>
           <br />
-          Web Platform
-        </h4>
-        <p className="text-secondary">
-          Pleased to meet you!
-          <br />
-          I mainly deal with Javascript and stuffs related with web.
-          <br />
-          Currently building great stuffs with the awesome team at <a href="https://www.tokopedia.com">Tokopedia</a>.
-          <br />
-          <br />
-          Feel free to reach me anytime to have a nice chat together 🍻
-        </p>
-      </div>
-      <div className={`${styles.styMainInfo} ${styles.styContactInfo}`}>
-        <h2 className="main-gradient">Get In Touch</h2>
-        <div className="text-secondary">
-          <p>
-            The universe is a pretty big place.
+          <h4 className="main-gradient">
+            Software Engineer
             <br />
-            You can find me on:
+            Web Platform
+          </h4>
+          <p>
+            Pleased to meet you!
+            <br />
+            I mainly deal with Javascript and stuffs related with web.
+            <br />
+            Currently building great stuffs with the awesome team at <a href="https://www.tokopedia.com">Tokopedia</a>.
+            <br />
+            <br />
+            Feel free to reach me anytime to have a nice chat together 🍻
           </p>
-          <ul>
-            <li>
-              <a href="https://www.linkedin.com/in/josteph22/">
-                <Linkedin /> @josteph22
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/josteph">
-                <GitHub /> @josteph
-              </a>
-            </li>
-            <li>
-              <a href="https://twitter.com/jostephhh">
-                <Twitter /> @jostephhh
-              </a>
-            </li>
-          </ul>
         </div>
+        <div className={`${styles.styMainInfo} ${styles.styContactInfo}`}>
+          <h4 className="main-gradient">Get In Touch</h4>
+          <div>
+            <p>
+              The universe is a pretty big place.
+              <br />
+              You can find me on:
+            </p>
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/josteph22/">
+                  <Linkedin /> @josteph22
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/josteph">
+                  <GitHub /> @josteph
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/jostephhh">
+                  <Twitter /> @jostephhh
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <div className={styles.styBlogInfo}>
+        <h2>Blog</h2>
+
+        {docs.map((doc) => (
+          <div className={styles.styBlogDisplay} key={doc.slug}>
+            <h3>
+              <Link passHref href={`/blog/${doc.slug}`}>
+                <a className="ani-link">{doc.meta.title}</a>
+              </Link>
+            </h3>
+            <p className="font-sm">{doc.meta.published}</p>
+            <p>{doc.meta.description}</p>
+          </div>
+        ))}
       </div>
     </main>
   );
