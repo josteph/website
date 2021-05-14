fb58475d054544dcf76c88ff51cd73bcb0d6a8ec
Joshua Stephen <jostephen22@yahoo.com>
2021-05-14 19:37:33 +0700

    feat: add dark mode detection

---
+++ components/Layout/index.tsx
@@ -0,0 +1,30 @@
+import React, { Fragment, useEffect } from 'react';
+import type { ReactNode } from 'react';
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  useEffect(() => {
+    let mql: MediaQueryList;
+
+    const handler = (event: MediaQueryListEvent | MediaQueryList) => {
+      document.documentElement.setAttribute('data-theme', event.matches ? 'dark' : 'light');
+    };
+
+    if (window.matchMedia) {
+      mql = window.matchMedia('(prefers-color-scheme: dark)');
+
+      handler(mql);
+
+      mql.addEventListener('change', handler);
+    }
+
+    return () => {
+      if (mql) {
+        mql.removeEventListener('change', handler);
+      }
+    };
+  }, []);
+
+  return <Fragment>{children}</Fragment>;
+};
+
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react';
+import Layout from '@components/Layout';
 import Header from '@components/Header';
 import { func, object } from 'prop-types';
 import '@styles/globals.scss';
@@ -8,10 +9,10 @@ import type { AppProps } from 'next/app';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <Header />
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
 
